99afa3c536f166900829037f56162462c767d3d8
LittleBigOwI <syl20.row@gmail.com>
2023-02-04 09:27:20 +0100

    Added main web app (demo)

---
+++ src/main/java/com/cmiurca/saintseiyadeckbuilding/App.java
@@ -0,0 +1,45 @@
+package com.cmiurca.saintseiyadeckbuilding;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	
+	/** 
+	 * Application start point
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	
+	/** 
+	 * Demo home page, returns a blank page with Hello World on it
+	 * (localhost:8080/)
+	 * @return String
+	 */
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+	
+	/** 
+	 * Demo for JSON data
+	 * (localhost:8080/j/)
+	 * @return List<String>
+	 */
+	@GetMapping("/j/")
+	public List<String> helloWorldList() {
+		return List.of("Hello", "World");
+	}
+
+}
---
+++ src/test/java/com/cmiurca/saintseiyadeckbuilding/SaintseiyadeckbuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmiurca.saintseiyadeckbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintseiyadeckbuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
